2b2145b81b5a996ad920bd0bd6d459356f0840e7
Harsh <apple@apples-MacBook-Air.local>
2024-12-18 18:08:03 +0530

    Made the ProductInfo and ProductList page react to the changes in the url

--- frontend/src/pages/ProductInfo.vue
+++ frontend/src/pages/ProductInfo.vue
@@ -1,21 +1,24 @@
 <template>
     <NavBar />
-    <ProductDetails :productName="productName"/>
-    <section aria-labelledby="reviews-heading" class="mx-auto max-w-7xl overflow-hidden">
-        <!-- <h2 id="reviews-heading" class="text-lg font-medium text-gray-900 sm:px-6 lg:px-8">Recent reviews</h2> -->
-        <ProductReview :productName="productName"/>
-    </section>
-    <div v-if="relatedItems.length">
-      <section aria-labelledby="related-heading" class="mx-auto max-w-7xl overflow-hidden">
-          <h2 id="related-heading" class="text-lg font-medium text-gray-900 sm:px-6 lg:px-8">Customers also purchased</h2>
-          <ProductsGrid :products="relatedItems"/>
+    <div :key="key">
+      <ProductDetails :productName="productName"/>
+      <section aria-labelledby="reviews-heading" class="mx-auto max-w-7xl overflow-hidden">
+          <!-- <h2 id="reviews-heading" class="text-lg font-medium text-gray-900 sm:px-6 lg:px-8">Recent reviews</h2> -->
+          <ProductReview :productName="productName"/>
       </section>
+      <div v-if="relatedItems.length">
+        <section aria-labelledby="related-heading" class="mx-auto max-w-7xl overflow-hidden">
+            <h2 id="related-heading" class="text-lg font-medium text-gray-900 sm:px-6 lg:px-8">Customers also purchased</h2>
+            <ProductsGrid :products="relatedItems"/>
+        </section>
+      </div>
     </div>
     <Footer />
 </template>
   
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch, computed } from 'vue'
+import { useRoute } from 'vue-router';
 import NavBar from '../components/NavBar.vue';
 import ProductDetails from '../components/ProductDetails.vue';
 import ProductReview from '../components/ProductReview.vue';
@@ -30,24 +33,35 @@ const props = defineProps({
     }
 });
 onMounted(() => {
-    fetchRelatedItems()
+    fetchRelatedItems(props.productName)
 });
+const route = useRoute();
+const key = computed(() => route.fullPath);
+const isLoading = ref(false)
 const useInternalServices = internalServices();
-const productName = props.productName;
+const productName = ref(props.productName);
 const relatedItems = ref([]);
-const fetchRelatedItems = async () => {
+const fetchRelatedItems = async (itemName) => {
+  isLoading.value = true;
+  relatedItems.value = [];
   try {
     const data = await useInternalServices.getRelatedItems.fetch({
-      "item_name": props.productName
+      "item_name": itemName
     });
     relatedItems.value = data
-    console.log("-------------", relatedItems.value)
-    // isLoading.value = false
   } catch (error) {
     console.error(`Failed to fetch related products for ${props.productName}:`, error);
+    isLoading.value = false;
   }
 
 };
+watch(
+  () => props.productName,
+  (newProduct) => {
+    productName.value = newProduct; 
+    fetchRelatedItems(newProduct)
+  }
+);
 
 
 </script>
--- frontend/src/pages/ProductList.vue
+++ frontend/src/pages/ProductList.vue
@@ -1,13 +1,18 @@
 <template>
     <NavBar />
-    <div class="mx-auto max-w-7xl overflow-hidden px-4 pt-8 sm:px-6 lg:px-8">
-        <h2 class="text-2xl font-bold tracking-tight text-gray-900">{{ categoryName }}</h2>
+    <div :key="key">
+        <div class="mx-auto max-w-7xl overflow-hidden px-4 pt-8 sm:px-6 lg:px-8">
+            <h2 class="text-2xl font-bold tracking-tight text-gray-900">{{ categoryName }}</h2>
+        </div>
+        <ProductFilters :categoryName="categoryName" :subCategoryName="subCategoryName"/>
     </div>
-    <ProductFilters :categoryName="categoryName" :subCategoryName="subCategoryName"/>
     <Footer />
+    
 </template>
   
 <script setup>
+import { ref, onMounted, watch, computed } from 'vue'
+import { useRoute } from 'vue-router';
 import NavBar from '../components/NavBar.vue';
 import ProductFilters from '../components/ProductFilters.vue';
 import Footer from '../components/Footer.vue';
@@ -22,9 +27,18 @@ const props = defineProps({
         required: false
     }
 });
+const route = useRoute();
+const key = computed(() => route.fullPath);
+const categoryName = ref(props.categoryName);
+const subCategoryName = ref(props.subCategoryName);
 
-const categoryName = props.categoryName
-const subCategoryName = props.subCategoryName
+watch(
+  [() => props.categoryName, () => props.subCategoryName],
+  ([newCategoryName, newSubCategoryName]) => {
+    categoryName.value = newCategoryName;
+    subCategoryName.value = newSubCategoryName;
+  }
+);
 
 </script>
   
